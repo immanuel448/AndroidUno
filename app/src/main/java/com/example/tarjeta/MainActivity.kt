611562b8a8a11a6c1f226680ccbe654b2603431e
Emmanuel Mendoza <emmanuelmendoza448@gmail.com>
2025-02-15 22:10:04 -0600

    Primer commit del proyecto Android

---
+++ app/src/main/java/com/example/tarjeta/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.tarjeta
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.tarjeta.ui.theme.TarjetaTheme
+
+var nombre: String = "Emmanuel"
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TarjetaTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = nombre,
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Surface(color = Color.Cyan) {
+        Text(
+            text = "Hola, mi nombre es: $name!",
+            modifier = Modifier.padding(25.dp),
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TarjetaTheme {
+        Greeting(nombre)
+    }
+}
