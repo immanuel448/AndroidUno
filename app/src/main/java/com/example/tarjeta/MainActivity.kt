5bcf2e98de508d658e5b3baef5ba3a8c53bd8fdb
Emmanuel Mendoza <emmanuelmendoza448@gmail.com>
2025-02-20 21:45:19 -0600

    git commit -m "Segundo Commit"

--- app/src/main/java/com/example/tarjeta/MainActivity.kt
+++ app/src/main/java/com/example/tarjeta/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -14,9 +15,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.tarjeta.ui.theme.TarjetaTheme
+import com.example.tarjeta.ui.theme.    TarjetaTheme
 
 var nombre: String = "Emmanuel"
+var rojito: Color = Color.Green
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,7 +27,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             TarjetaTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
+                    Snoopy(
                         name = nombre,
                         modifier = Modifier.padding(innerPadding)
                     )
@@ -36,19 +38,25 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Surface(color = Color.Transparent) {
+fun Snoopy(name: String, modifier: Modifier = Modifier) {
+    Surface(
+        modifier = Modifier
+            .padding(20.dp),
+        color = rojito){
         Text(
             text = "Hola, mi nombre es: $name!",
-            modifier = Modifier.padding(25.dp),
+            modifier = Modifier
+                .padding(20.dp)
+                .background(Color.Cyan),
+                color = Color.Blue,
         )
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, name = "titulo?", showSystemUi = true)
 @Composable
-fun GreetingPreview() {
+fun funcionCambiadaPreview() {
     TarjetaTheme {
-        Greeting(nombre)
+        Snoopy(nombre)
     }
 }
